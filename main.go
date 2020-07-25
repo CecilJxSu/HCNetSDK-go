5ee2d10af8fd730a329375966e84be6eb0ec9074
cecil <ceciljxsu@gmail.com>
2020-07-25 22:08:19 +0800

    添加登陆例子

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	hik "./hikvision"
+	hik "HCNetSDK-go/hikvision"
 	"fmt"
 	"strconv"
+	"unsafe"
 )
 
 func main() {
@@ -15,4 +16,29 @@ func main() {
 		fmt.Println("Not success, error code: " + strconv.FormatInt(int64(errCode), 10))
 		return
 	}
+
+	// 登陆参数
+	loginInfo := hik.NET_DVR_USER_LOGIN_INFO{}
+	deviceInfo := hik.NET_DVR_DEVICEINFO_V40{}
+
+	copy(loginInfo.SDeviceAddress[:], "192.168.8.110")
+	loginInfo.WPort = 8000
+	loginInfo.ByUseTransport = 0
+	loginInfo.BUseAsynLogin = 1
+	copy(loginInfo.SUserName[:], "<your username>")
+	copy(loginInfo.SPassword[:], "<your password>")
+	// 异步登陆回调函数
+	loginInfo.CbLoginResult = func(lUserID int, dwResult uint32, lpDeviceInfo hik.LPNET_DVR_DEVICEINFO_V30, pUser unsafe.Pointer) {
+		if 1 == dwResult {
+			fmt.Println("异步登陆成功，userId: ", strconv.Itoa(lUserID))
+			fmt.Println("设备序列号: ", string(lpDeviceInfo.SSerialNumber[:]))
+		} else {
+			fmt.Println("异步登陆失败")
+		}
+	}
+	// 登陆
+	hik.NET_DVR_Login_V40(&loginInfo, &deviceInfo)
+
+	hangOn := make(chan int)
+	<-hangOn
 }
